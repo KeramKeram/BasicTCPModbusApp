e99afd6a9f9ef67a721ae0e00dd5db9e076efd55
KeramKeram <rev@autograf.pl>
2022-04-19 21:28:07 +0200

    Ui: Add empty window.

--- src/BasicTCPModbusApp/Program.cs
+++ src/BasicTCPModbusApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Terminal.Gui;
+namespace BasicTCPModbusApp;
+
+public class MainApp
+{
+    static void Main()
+    {
+        Application.Init();
+        var top = Application.Top;
+        MainUI.MainUi mainUiApp = new MainUI.MainUi()
+        {
+            X = 0,
+            Y = 1,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
+        };
+        top.Add(mainUiApp);
+        Application.Run();
+    }
+}
---
+++ src/BasicTCPModbusApp/UI/MainUi.cs
@@ -0,0 +1,13 @@
+using Terminal.Gui;
+using NStack;
+
+namespace BasicTCPModbusApp.MainUI;
+
+public class MainUi : Window
+{
+
+    public MainUi() : base("MyApp")
+    {
+
+    }
+}

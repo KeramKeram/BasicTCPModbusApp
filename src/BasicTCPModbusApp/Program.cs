1f34f9115c3d33e0dbe7ed431bc8a4fd72b3599d
KeramKeram <rev@autograf.pl>
2022-04-22 21:57:34 +0200

    Window: Add menu item.

--- src/BasicTCPModbusApp/Program.cs
+++ src/BasicTCPModbusApp/Program.cs
@@ -15,6 +15,29 @@ public class MainApp
             Height = Dim.Fill()
         };
         top.Add(mainUiApp);
+
+        top.Add(CreateMenuBar(top));
+
         Application.Run();
+    }
+
+    private static MenuBar CreateMenuBar(Toplevel top)
+    {
+        var menu = new MenuBar(new MenuBarItem[] {
+            new MenuBarItem ("_File", new MenuItem [] {
+                new MenuItem ("_Quit", "", () => { if (Quit ()) top.Running = false; })
+            }),
+            new MenuBarItem ("Preference", new MenuItem [] {
+                new MenuItem ("Network", "", null),
+                new MenuItem ("View", "", null),
+            })
+        });
+        return menu;
+    }
+
+    static bool Quit()
+    {
+        var n = MessageBox.Query(50, 7, "Quit Program", "Are you sure you want to quit this demo?", "Yes", "No");
+        return n == 0;
     }
 }

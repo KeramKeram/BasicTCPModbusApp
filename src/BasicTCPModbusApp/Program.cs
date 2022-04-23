d84f61d57bb9755704bb49dd189c8f908aa40196
KeramKeram <rev@autograf.pl>
2022-04-23 22:45:23 +0200

    View: Add empty dialog for network prferences.

--- src/BasicTCPModbusApp/Program.cs
+++ src/BasicTCPModbusApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Terminal.Gui;
+
 namespace BasicTCPModbusApp;
 
 public class MainApp
@@ -14,22 +15,30 @@ public class MainApp
             Width = Dim.Fill(),
             Height = Dim.Fill()
         };
-        top.Add(mainUiApp);
 
-        top.Add(CreateMenuBar(top));
+        Action showDialogDelegate = CreateNetworkDialogDelegate();
+
+        top.Add(mainUiApp);
+        top.Add(CreateMenuBar(top, showDialogDelegate));
 
         Application.Run();
     }
 
-    private static MenuBar CreateMenuBar(Toplevel top)
+    private static MenuBar CreateMenuBar(Toplevel top, Action ac)
     {
-        var menu = new MenuBar(new MenuBarItem[] {
-            new MenuBarItem ("_File", new MenuItem [] {
-                new MenuItem ("_Quit", "", () => { if (Quit ()) top.Running = false; })
+        var menu = new MenuBar(new MenuBarItem[]
+        {
+            new MenuBarItem("_File", new MenuItem[]
+            {
+                new MenuItem("_Quit", "", () =>
+                {
+                    if (Quit()) top.Running = false;
+                })
             }),
-            new MenuBarItem ("Preference", new MenuItem [] {
-                new MenuItem ("Network", "", null),
-                new MenuItem ("View", "", null),
+            new MenuBarItem("Preference", new MenuItem[]
+            {
+                new MenuItem("Network", "", ac),
+                new MenuItem("View", "", null)
             })
         });
         return menu;
@@ -39,5 +48,35 @@ public class MainApp
     {
         var n = MessageBox.Query(50, 7, "Quit Program", "Are you sure you want to quit this demo?", "Yes", "No");
         return n == 0;
+    }
+
+    static Action CreateNetworkDialogDelegate()
+    {
+        return new Action(() =>
+        {
+            int width = int.Parse("10");
+            int height = int.Parse("10");
+            int numButtons = int.Parse("1");
+
+            var buttons = new List<Button>();
+            var clicked = -1;
+            for (int i = 0; i < numButtons; i++)
+            {
+                var buttonId = i;
+                //var button = new Button (btnText [buttonId % 10],
+                //	is_default: buttonId == 0);
+                var button = new Button("OK");
+                button.Clicked += () =>
+                {
+                    clicked = buttonId;
+                    Application.RequestStop();
+                };
+                buttons.Add(button);
+            }
+
+            var dialog = new Dialog("Test Title", width, height,
+                buttons.ToArray());
+            Application.Run(dialog);
+        });
     }
 }

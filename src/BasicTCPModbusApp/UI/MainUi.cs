74567114e67b402168d5c39b32294e8df4f5e984
KeramKeram <rev@autograf.pl>
2022-05-02 21:29:40 +0200

    MainView: Add bar with display lenght, radio with type of reg. and pooling button.

--- src/BasicTCPModbusApp/UI/MainUi.cs
+++ src/BasicTCPModbusApp/UI/MainUi.cs
@@ -5,9 +5,70 @@ namespace BasicTCPModbusApp.MainUI;
 
 public class MainUi : Window
 {
-
     public MainUi() : base("MyApp")
     {
+        var subWin = new Window($"Centered Sub Window with 10 character margin")
+        {
+            X = Pos.Center(),
+            Y = 1,
+            Width = Dim.Fill(5),
+            Height = 8
+        };
 
+        var displayLengthLabel = new Label($"Display Length:");
+        subWin.Add(displayLengthLabel);
+
+        var displayLenghtValue = new TextField
+        {
+            X = Pos.Right(displayLengthLabel) + 1,
+            Y = displayLengthLabel.Y,
+            Width = 10,
+            Height = 10,
+            Text = ""
+        };
+
+        subWin.Add(displayLenghtValue);
+
+        var registerTypeLabel = new Label
+        {
+            X = Pos.Right(displayLenghtValue) + 10,
+            Y = displayLengthLabel.Y,
+            Text = "Register Type:"
+        };
+        subWin.Add(registerTypeLabel);
+
+        var registerTypeRadio = new RadioGroup(new ustring[]
+            { "Coils Reg.", "Input Status Reg.", "Input Register Reg.", "Holding Reg." })
+        {
+            X = Pos.Right(registerTypeLabel) + 1,
+            Y = displayLengthLabel.Y,
+            SelectedItem = 0,
+        };
+        subWin.Add(registerTypeRadio);
+
+        var statusLabel = new Label
+        {
+            X = Pos.Right(registerTypeRadio) + 1,
+            Y = registerTypeRadio.Y,
+            Text = "Status:"
+        };
+        subWin.Add(statusLabel);
+
+        var statusLabelValue = new Label
+        {
+            X = Pos.Right(statusLabel) + 1,
+            Y = registerTypeRadio.Y,
+            Text = ""
+        };
+        subWin.Add(statusLabelValue);
+
+        var poolingButton = new Button()
+        {
+            X = displayLengthLabel.X,
+            Y = Pos.Bottom(registerTypeRadio) + 1,
+            Text = "Start Pooling"
+        };
+        subWin.Add(poolingButton);
+        this.Add(subWin);
     }
 }

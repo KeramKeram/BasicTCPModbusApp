934663883c5e8be6bcd3069846fff30966b349fd
revcorey <rev@autograf.pl>
2022-05-23 22:08:02 +0200

    Modbus: Init modbus class.

---
+++ src/BasicTCPModbusApp/Modbus/ModbuClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using FluentModbus;
+
+namespace BasicTCPModbusApp.Modbus
+{
+    class ModbuClient
+    {
+        private ModbusTcpClient? _mClient;
+        public string _mAddress = "127.0.0.1";
+        public int _mPort = 5020;
+
+        ModbuClient()
+        {
+            _mClient = new ModbusTcpClient();
+            _mClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 502));
+        }
+    }
+}

3d68913b32b1d21beda5ae5862c9d2d2d889a905
revcorey <rev@autograf.pl>
2022-05-24 22:38:15 +0200

    Modbus: Implement reading functions in modbus client.

--- src/BasicTCPModbusApp/Modbus/ModbuClient.cs
+++ src/BasicTCPModbusApp/Modbus/ModbuClient.cs
@@ -5,14 +5,37 @@ namespace BasicTCPModbusApp.Modbus
 {
     class ModbuClient
     {
-        private ModbusTcpClient? _mClient;
+        private ModbusTcpClient _mClient = new ModbusTcpClient();
         public string _mAddress = "127.0.0.1";
         public int _mPort = 5020;
 
-        ModbuClient()
+        ModbuClient() => _mClient.Connect(new IPEndPoint(IPAddress.Parse(_mAddress), _mPort));
+
+        ~ModbuClient() => _mClient.Disconnect();
+
+        public int ReadHoldingRegister(int unitIdent, int startingAddres)
         {
-            _mClient = new ModbusTcpClient();
-            _mClient.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 502));
+            var intData = _mClient.ReadHoldingRegisters<int>(unitIdent, startingAddres, 1);
+            return intData[0];
+        }
+
+        public int ReadCoil(int unitIdent, int startingAddres)
+        {
+            var boolData = _mClient.ReadCoils(unitIdent, startingAddres, 1);
+            bool boolValue = ((boolData[0] >> 0) & 1) > 0;
+            return boolValue ? 1 : 0;
+        }
+
+        public int ReadDiscreteInputs(int unitIdent, int startingAddres)
+        {
+            var intData = _mClient.ReadDiscreteInputs(unitIdent, startingAddres, 1);
+            return intData[0];
+        }
+
+        public int ReadInputRegisters(int unitIdent, int startingAddres)
+        {
+            var intData = _mClient.ReadInputRegisters<int>(unitIdent, startingAddres, 1);
+            return intData[0];
         }
     }
 }

0b863d22efe5a93ce619435257fb355cf6b51eca
revcorey <rev@autograf.pl>
2022-05-30 22:15:50 +0200

    Interface: Add abstraction between UI and Modbus commands.

---
+++ src/BasicTCPModbusApp/Modbus/ModbusInvoker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicTCPModbusApp.Modbus
+{
+    public interface IInvoker
+    {
+        public void SetNetworkParameters(string ipAddress, int ipPort);
+    };
+    class ModbusInvoker : IInvoker
+    {
+        public ICommand<string>? _mCmdSetIpAddress { get; set; }
+        public ICommand<int>? _mCmdSetIpPort { get; set; }
+
+        public void SetNetworkParameters(string ipAddress, int ipPort)
+        {
+            _mCmdSetIpAddress?.setParameter(ipAddress);
+            _mCmdSetIpAddress?.Execute();
+            _mCmdSetIpPort?.setParameter(ipPort);
+            _mCmdSetIpPort?.Execute();
+        }
+    }
+}
